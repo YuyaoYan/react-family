7c305414909f61ac7d6a96421805197f2cf3aefa
闫煜瑶 <yanyy2@yonyou.com>
2020-11-11 16:45:12 +0800

    add bundle.js

---
+++ src/router/bundle.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Bundle extends Component {
+  state = {
+    // short for "module" but that's a keyword in js, so "mod"
+    mod: null,
+  };
+
+  componentWillMount() {
+    console.log('this.props', this.props);
+
+    this.load(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.load !== this.props.load) {
+      this.load(nextProps);
+    }
+  }
+
+  load(props) {
+    console.log('props', props);
+    this.setState({
+      mod: null,
+    });
+    props.load((mod) => {
+      console.log('mod', mod);
+      this.setState({
+        // handle both es imports and cjs
+        mod: mod.default ? mod.default : mod,
+      });
+    });
+  }
+
+  render() {
+    return this.props.children(this.state.mod);
+  }
+}
+
+export default Bundle;
